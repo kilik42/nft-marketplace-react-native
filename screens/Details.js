36ad9b3263886d737f27b66146640a027aa92963
kilik42 <marvinevins@yahoo.com>
2022-04-03 17:53:05 -0500

    working on implementing data

---
+++ screens/Details.js
@@ -0,0 +1,10 @@
+import React from 'react'
+//rafce is a shortcut for react component
+import {Text} from 'react-native';
+const Details = () => {
+  return (
+    <Text> Details </Text>
+  )
+}
+
+export default Details
---
+++ screens/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Text, View, SafeAreaView, FlatList} from 'react-native';
+import { useState } from 'react';
+import{NFTCard, HomeHeader, FocusedStatusBar} from '../components';
+import {COLORS, NFTData} from '../constants';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.background}}>
+       
+       <FocusedStatusBar background={COLORS.primary}/>
+       <View style={{flex: 1}}>
+            <View style={{zIndex:0}}>
+                <FlatList 
+                data = {NFTData}
+                renderItem = {({item}) => <NFTCard data={item.name}/>}
+                keyExtractor = {(item) => item.id}
+                numColumns = {2}
+                
+                />
+            </View>
+        </View>
+    </SafeAreaView>
+
+  )
+}
+
+export default Home
